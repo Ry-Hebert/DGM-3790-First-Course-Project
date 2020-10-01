0fd505adb55e4358f72ae9446479400e8340b96c
Ryan <ryan@thecpafirm.com>
2020-09-30 20:39:30 -0600

    fixed ship data dis

--- project/src/FleetDis.js
+++ project/src/FleetDis.js
@@ -1,5 +1,23 @@
 import React, { Component } from 'react'
 import FleetGrab from './FleetGrab'
+import ShipList from './assets/fleetyard'
+
+const ShipData = val => {
+    if(val === 1){
+        return (
+            ShipList.forEach(element => {
+                return(
+                <div className='shipCard'>
+                    <h4>{element.name}</h4>
+                </div>
+                )
+            })
+        )
+    }
+    else {
+        return (<div></div>)
+    }
+}
 
 class fleetDis extends Component {
     
@@ -11,27 +29,42 @@ class fleetDis extends Component {
         this.setState({
             displayTog: 1
         })
-    }
+        console.log(this.state.displayTog)
+        return (
+            <div className='objList'>
+                {
+                ShipList.forEach(element => {
+                    console.log('This is: ' + element.name)
+                    return (<div className='shipCard'>
+                        <h4>{element.name}</h4>
+                        </div>
+                    )
+                })
+                }
+            </div>
+    )} 
 
     shipDisplayToggle2 = () =>{
         this.setState({
-            displayTog: 1
+            displayTog: 2
         })
+        console.log(this.state.displayTog)
     }
 
     render(){
+        let disTog = this.state.displayTog
         return (
             <main>
                 <div>
                     <h1>Star Citizen Ship List</h1>
-                    <h2>This App displays a list of ships from the game Star Citizen</h2>
+                    <h2>This App displays a list of ships from the game Star Citizen, which is produced by Roberts Space Industries</h2>
                 </div>
                 <div>
                     <button onClick={this.shipDisplayToggle1}>Display all Ships</button>
                     <button onClick={this.shipDisplayToggle2}>Display Ships With Brochure</button>
                 </div>
                 <div>
-                    <FleetGrab val={this.state.displayTog}></FleetGrab>
+                    <ShipData val={disTog}></ShipData>
                 </div>
             </main>
         )
--- project/src/FleetGrab.js
+++ project/src/FleetGrab.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import ShipList from './assets/fleetyard'
+import FleetDis from './FleetDis'
 
 const ShipData = val => {
-    if(val == 1){
+    if(val === 1){
         return (
             <div className='objList'>
-            {
+                {
                 ShipList.forEach(element => {
-                    return (
-                        <div className='shipCard'>
-                            <h4>{element.name}</h4>
+                    return (<div className='shipCard'>
+                        <h4>{element.name}</h4>
                         </div>
                     )
                 })
-            }
+                }
             </div>
         )
     }
