f83b52a30abd25fa4a0ebfcb69a1cddffb3b8d88
Ryan <ryan@thecpafirm.com>
2020-10-02 13:02:33 -0600

    maybe this fixes things

---
+++ project/build/precache-manifest.7d0f13b2ab86e968a367bd077e5dd74b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b9ad96b9c19e2506a7b101b25af37c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e860642dce9a0c786a2a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae34a39afa72e69c3689",
+    "url": "/static/js/2.7d0df3d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d0df3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e860642dce9a0c786a2a",
+    "url": "/static/js/main.26b23bed.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  }
+]);
